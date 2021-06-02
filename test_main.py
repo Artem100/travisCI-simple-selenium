cf820207ce045b9b2103cce70366130cfe1244ae
Artem <you@example.com>
2021-06-02 17:52:38 +0300

    Add environment value

--- test_main.py
+++ test_main.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 import allure
@@ -6,7 +7,11 @@ from selenium import webdriver
 @allure.title("First test")
 def test_01():
     driver = webdriver.Firefox()
-    driver.get("https://stepik.org/lesson/25969/step/12")
+    # driver.get("https://stepik.org/lesson/25969/step/12")
+    try:
+        driver.get(os.environ["URL_ENV"])
+    except KeyError:
+        driver.get("https://www.google.com/")
     time.sleep(7)
     textarea = driver.find_element_by_css_selector(".textarea")
     textarea.send_keys("get()")
